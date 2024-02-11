81d197039e7864ab29d662edc63d65319cb137f6
tev <tevelshorz@gmail.com>
2024-02-11 16:38:25 +0800

    Code updates

--- script.js
+++ script.js
@@ -1,1 +1,52 @@
+let password = document.querySelector("#myPassword");
+let show = document.querySelector(".show");
+let container = document.querySelector(".container");
 
+document.addEventListener("keyup", function(e){
+    let passwordValue = password.value;
+    let strengthValue = strength(passwordValue);
+    
+    if (strengthValue <= 2){
+        container.classList.add("weak");
+        container.classList.remove("medium");
+        container.classList.remove("strong");
+    } else if (strengthValue <= 4){
+        container.classList.remove("weak");
+        container.classList.add("medium");
+        container.classList.remove("strong");
+    } else {
+        container.classList.remove("weak");
+        container.classList.remove("medium");
+        container.classList.add("strong");
+    }
+});
+
+show.onclick = function(){
+    if (password.type === "password"){
+        password.setAttribute("type", "text");
+        show.classList.add("hide");
+    } else {
+        password.setAttribute("type", "password");
+        show.classList.remove("hide");
+    }
+};
+
+function strength(password){
+    let i = 0;
+    if (password.length > 6){
+        i++;
+    }
+    if (password.length >= 10){ 
+        i++;
+    }
+    if (/[A-Z]/.test(password)){
+        i++;
+    }
+    if (/[0-9]/.test(password)){
+        i++;
+    }
+    if (/[A-Za-z0-9]/.test(password)){ 
+        i++;
+    }
+    return i;
+}
